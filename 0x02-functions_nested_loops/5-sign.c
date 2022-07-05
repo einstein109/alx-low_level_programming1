86188a0e42158a0e3f12b7334813c6aafbb40c63
einstein109 <107047869+einstein109@users.noreply.github.com>
2022-07-05 16:34:28 +0300

    task

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,25 +1,23 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * print_sign - prints the sign of a number
- * @n: the int to check
- * Return: 1 and prints + if n is greater than zero
- * 0 and prints 0 if n is zero
- * -1 and prints - if n is less than zero
+ * print_sign - Print the sign of a number
+ *@n: integer input
+ *
+ * Return: -1 (Negative) 0 (Zero) 1 (Positive)
  */
 int print_sign(int n)
 {
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
 	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
-	} else if (n == 0)
-	{
-		_putchar(48);
-		return (0);
-	} else if (n < 0)
-	{
-		_putchar('-');
 	}
-		return (-1);
+	_putchar('0');
+	return (0);
 }
