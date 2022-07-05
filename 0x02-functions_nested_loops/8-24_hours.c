4c7dda8775a4917c059bbe95ae8e2a0296f5ae52
einstein109 <107047869+einstein109@users.noreply.github.com>
2022-07-05 16:37:59 +0300

    task

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,28 +1,22 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer
- * starting from 00:00 to 23:59
+ * jack_bauer - print every minute and hour of the day
  */
 void jack_bauer(void)
 {
-	int i, j;
+	int hour, min;
 
-	i = 0;
-
-	while (i < 24)
+	for (hour = 0; hour < 24; hour++)
 	{
-		j = 0;
-		while (j < 60)
+		for (min = 0; min < 60; min++)
 		{
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
 			_putchar(':');
-			_putchar((j / 10) + '0');
-			_putchar((j % 10) + '0');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
 			_putchar('\n');
-			j++;
 		}
-		i++;
 	}
 }
