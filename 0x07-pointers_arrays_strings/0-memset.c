072ac9704dd95421cd40be13847985edf0c9ef83
einstein109 <107047869+einstein109@users.noreply.github.com>
2022-07-18 13:29:41 +0300

    task

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,91 +1,23 @@
-nclude "main.h"
-
-#include <stdio.h>
-
-
-
+#include "main.h"
 /**
- *
- *  * simple_print_buffer - prints buffer in hexa
- *
- *   * @buffer: the address of memory to print
- *
- *    * @size: the size of the memory to print
- *
- *     *
- *
- *      * Return: Nothing.
- *
- *       */
+ *_memset -   fills memory with a constant byte
+ *@s: pointer block of memory to fill
+ *@b: value to set
+ *@n: bytes of the memory
+ *Return: dest
+ */
 
-void simple_print_buffer(char *buffer, unsigned int size)
+char *_memset(char *s, char b, unsigned int n)
 
 {
+	unsigned int i;
 
-	        unsigned int i;
+	/*Delacring FOR*/
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b; /*add 1 position s*/
 
+	} /*END FOR*/
 
-
-		        i = 0;
-
-			        while (i < size)
-
-					        {
-
-							                if (i % 10)
-
-										                {
-
-													                        printf(" ");
-
-																                }
-
-									                if (!(i % 10) && i)
-
-												                {
-
-															                        printf("\n");
-
-																		                }
-
-											                printf("0x%02x", buffer[i]);
-
-													                i++;
-
-															        }
-
-				        printf("\n");
-
-}
-
-
-
-/**
- *
- *  * main - check the code
- *
- *   *
- *
- *    * Return: Always 0.
- *
- *     */
-
-int main(void)
-
-{
-
-	    char buffer[98] = {0x00};
-
-
-
-	        simple_print_buffer(buffer, 98);
-
-		    _memset(buffer, 0x01, 95);
-
-		        printf("-------------------------------------------------\n");
-
-			    simple_print_buffer(buffer, 98);    
-
-			        return (0);
-
+	return (s);
 }
