0bed399399014dd0ddc534b6848d6c99efe4997a
einstein109 <107047869+einstein109@users.noreply.github.com>
2022-07-22 06:25:45 -0700

    task

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,41 +1,28 @@
-include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-
 /**
- * main - cheks the fuction.
- * @argc: couts the arguments passed to the function.
- * @argv: gives the value for the argument passed to the function.
- *
- * Return: Returns always success.
+ * main - adds positive numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
-
 int main(int argc, char *argv[])
 {
-int sum;
-int count;
-int i;
+	int i, j, add = 0;
 
-count = 1;
-sum = 0;
-if (argc == 1)
-{
-printf("0\n");
-return (0);
-}
-while (count < argc)
-{
-for (i = 0; argv[count][i] != '\0'; i++)
-{
-if (!(isdigit(argv[count][i])))
-{
-printf("Error\n");
-return (1);
-}
-}
-sum += atoi(argv[count]);
-count++;
-}
-printf("%d\n", sum);
-return (0);
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		add += atoi(argv[i]);
+	}
+	printf("%d\n", add);
+	return (0);
 }
