2d9e566e038e0babcdec100c7b75d3132a69f69b
einstein109 <107047869+einstein109@users.noreply.github.com>
2022-07-25 06:10:32 -0700

    task

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
-#define MAIN_H
-#include <stdio.h>
-#include <stdlib.h>
+#define MAINN_H
+
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
@@ -9,4 +8,6 @@ char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
+char **strtow(char *str);
+
 #endif
